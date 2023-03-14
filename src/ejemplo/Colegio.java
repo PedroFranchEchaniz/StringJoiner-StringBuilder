a28e16fe0793a5f381f1ab3818616b5423755d8c
Pedro Franch <pedrofe.av@gmail.com>
2023-03-14 13:28:29 +0100

    ejemplos

---
+++ src/ejemplo/Colegio.java
@@ -0,0 +1,14 @@
+package ejemplo;
+
+public class Colegio {
+
+	public void renombrarAlumn(Alumno a, String nombre) {
+		a.setNombre(nombre);
+	}
+	
+	public void cambiarEdad(Alumno a, int edad) {
+		a.setEdad(edad);
+	}
+	
+	
+}
